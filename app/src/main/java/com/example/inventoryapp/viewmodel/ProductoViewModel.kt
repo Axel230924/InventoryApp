c4c95088192cccc0a06c3293e15cc9813b53c6e1
ElkiMontes <elkinmonteskv1@gmail.com>
2026-08-20 16:34:56 -0600

    ViewModel

--- app/src/main/java/com/example/inventoryapp/viewmodel/ProductoViewModel.kt
+++ app/src/main/java/com/example/inventoryapp/viewmodel/ProductoViewModel.kt
@@ -1,4 +1,14 @@
 package com.example.inventoryapp.viewmodel
 
-class ProductoViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import com.inventoryapp.data.entity.Producto
+import com.example.inventoryapp.data.repository.ProductoRepository
+
+class ProductoViewModel(private val repository:ProductoRepository):ViewModel(){
+    fun guardarProducto(producto: Producto)
+    {
+    viewModelScope.launch{repository.Insertar(producto)}
+    }
 }
