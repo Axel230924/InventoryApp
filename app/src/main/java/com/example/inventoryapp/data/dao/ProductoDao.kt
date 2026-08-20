3c1becaf461ddbe217593392498ed9d687e8aaac
ElkiMontes <elkinmonteskv1@gmail.com>
2026-08-20 15:50:55 -0600

    Dao

--- app/src/main/java/com/example/inventoryapp/data/dao/ProductoDao.kt
+++ app/src/main/java/com/example/inventoryapp/data/dao/ProductoDao.kt
@@ -1,4 +1,18 @@
 package com.example.inventoryapp.data.dao
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.inventoryapp.data.entity.Producto
 
-class ProductoDao {
+@Dao
+interface ProductoDao {
+    @Insert suspend fun Insertar(producto: Producto)
+
+    @Update suspend fun Actualizar(producto: Producto)
+
+    @Delete suspend fun Eliminar(producto: Producto)
+
+    @Query("SELECT * FROM Productos") suspend fun ObtenerTodos(): List<Producto>
 }
