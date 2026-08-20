a8ba46574aaf2344bdb49a1558ab1a161ad86624
Axel <axelmolinares07@gmail.com>
2026-08-20 16:22:48 -0600

    Creación del repository

--- app/src/main/java/com/example/inventoryapp/data/entity/Producto.kt
+++ app/src/main/java/com/example/inventoryapp/data/entity/Producto.kt
@@ -1,14 +1,14 @@
 package com.inventoryapp.data.entity
-import androidx.room.Entity
-import androidx.room.PrimaryKey
+import androidx.room.Entity // Importamos la entidad de la base de datos
+import androidx.room.PrimaryKey // Importamos el atributo Llave primaria
 
-@Entity(tableName = "productos")
-data class Producto(
-    @PrimaryKey(autoGenerate = true)
-    val id:Int = 0,
-    val nombre:String,
-    val precio:Double,
-    val cantidad:Int,
-    val categoria:String,
-    val imagen:String=""
+@Entity(tableName = "productos") // Definimos el nombre de nuestra tabla como una entidad
+data class Producto( // creamos una clase
+    @PrimaryKey(autoGenerate = true) // Le indicamos que la Llave primaria va a ser autoincrementable
+    val id:Int = 0, // Atributo Llave primaria
+    val nombre:String, // Nombre
+    val precio:Double, // Precio
+    val cantidad:Int, // Cantidad
+    val categoria:String, // Categoria a la que pertenece
+    val imagen:String="" // Campo imagen
 )
