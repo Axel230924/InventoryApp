4160923c41de1f06063ade2d10b9fe5d7b6c5513
ElkiMontes <elkinmonteskv1@gmail.com>
2026-08-20 18:32:52 -0600

    AvanzesCRUD(Update)

--- app/src/main/java/com/example/inventoryapp/viewmodel/ProductoViewModel.kt
+++ app/src/main/java/com/example/inventoryapp/viewmodel/ProductoViewModel.kt
@@ -1,14 +1,14 @@
-package com.example.inventoryapp.viewmodel
+package com.inventoryapp.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import com.inventoryapp.data.entity.Producto
-import com.example.inventoryapp.data.repository.ProductoRepository
+import com.inventoryapp.data.repository.ProductoRepository
 
 class ProductoViewModel(private val repository:ProductoRepository):ViewModel(){
     fun guardarProducto(producto: Producto)
     {
-    viewModelScope.launch{repository.Insertar(producto)}
+    viewModelScope.launch{repository.insertar(producto)}
     }
 }
