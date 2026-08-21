51351ff591ede38d09086ae0fef98ee535849420
Axel <axelmolinares07@gmail.com>
2026-08-20 21:56:27 -0600

    Guardar productos integrado

--- app/src/main/java/com/example/inventoryapp/ui/productos/ProductoAdapter.kt
+++ app/src/main/java/com/example/inventoryapp/ui/productos/ProductoAdapter.kt
@@ -9,7 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.inventoryapp.R
 
 class ProductoAdapter(
-    private val productos: List<Producto>
+    private val productoModels: List<ProductoModel>
 ) : RecyclerView.Adapter<ProductoAdapter.ProductoViewHolder>() {
 
     class ProductoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -38,16 +38,16 @@ class ProductoAdapter(
         position: Int
     ) {
 
-        val producto = productos[position]
+        val producto = productoModels[position]
 
         holder.imagen.setImageResource(producto.imagen)
         holder.nombre.text = producto.nombre
         holder.codigo.text = "Código: ${producto.codigo}"
-        holder.stock.text = "Stock: ${producto.stock}"
+        holder.stock.text = "Stock: ${producto.cantidad}"
         holder.precio.text = "${producto.precio}"
     }
 
     override fun getItemCount(): Int {
-        return productos.size
+        return productoModels.size
     }
 }
